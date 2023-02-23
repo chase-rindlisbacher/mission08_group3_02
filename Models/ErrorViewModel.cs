eb2fff492efb6be45b65ad5176d519b8911aae86
chase.rindlisbacher <cjrjazz20@gmail.com>
2023-02-23 15:31:22 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mission08_group3_02.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
